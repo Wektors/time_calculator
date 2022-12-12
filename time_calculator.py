0f2a0d1526812f030d5d63cbbf64cee0fc720115
Wektors <wiktorm16@gmail.com>
2022-12-12 22:19:30 +0100

    working adding

--- time_calculator.py
+++ time_calculator.py
@@ -1,7 +1,46 @@
-def add_time(start, duration):
+def add_time(start, duration, *day):
+
+    split_start = start.replace(':', " ").replace('AM', '00').replace('PM', '12').split()
+    
+    minutes_start = int(split_start[0]) * 60 + int(split_start[1]) + int(split_start[2]) * 60
+
+    split_duration = duration.split(':')
+
+    minutes_duration = int(split_duration[0]) * 60 + int(split_duration[1])
+
+    minutes_result = minutes_start + minutes_duration
+
+    days_result = 0
+
+    while minutes_result > 1440:
+        days_result += 1;
+        minutes_result -= 1440
+
+    while minutes_result >= 720:
+        am_pm = "PM"
+        minutes_result -= 720
+
+    else:
+        am_pm = "AM"
+
+    hours_result = 0
+
+    while minutes_result >= 60:
+        hours_result += 1
+        minutes_result -= 60
+
+    if hours_result == 0:
+        hours_result = "00"
+    
+    elif hours_result < 10:
+        hours_result = "0" + str(hours_result)
 
 
 
+    
 
+    time_result = str(hours_result) + ":" + str(minutes_result) + " " + am_pm
 
-    return new_time
+    return time_result
+
+print(add_time("2:05 PM", "1:05",))
